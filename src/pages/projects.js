829174cf273cd89121231202461e9e0977c291fc
Sanzhar Kiyassov <kiyassovs@gmail.com>
2024-12-22 16:30:54 +0500

    fixed header of pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,5 @@
-import styles from '../assets/css/projects.module.css';
+import styles from './projects.module.css';
+import ProjectNavbar from '../components/projectsComponents/addProjects';
 import Sidebar from '../components/sidebar';
 import Filters from '../components/projectsComponents/filters';
 import Card from '../components/projectsComponents/card'
@@ -6,41 +7,18 @@ import Card from '../components/projectsComponents/card'
 const Projects = () => {
     return (
       <div className={styles.container}>
-        <Sidebar />
+        <div className={styles.sidebar}>
+          <Sidebar /> 
+        </div>
         
-        <div className = {styles.project}>
-          <div className= {styles.project_header}>
-
-            <div className= {styles.project_header_top}>
-              <div className= {styles.header_left}>
-                <h2>Проекты</h2>
-                <p>113</p>
-              </div>
-              <div className= {styles.header_right}>
-                <button className= {styles.add_button}>
-                  <div className = {styles.add_button_text}>
-                    <div className={styles.add_button_text}>
-                      <div className= {styles.add_button_text_left}>
-                        <p id={styles.plus}>+</p>
-                      </div>  
-                      <div className= {styles.add_button_text_right}>
-                        <p>Добавить</p>
-                      </div>
-                    </div>
-                  </div>
-                  </button>
-              </div>
-            </div>
-
-            <div className= {styles.project_header_bot}>
-              <Filters />
-              <Card />
-             
-            </div>
-
-          </div>
+        <div className={styles.inner_container}>
+          <ProjectNavbar />
+        
+          <Filters />
+          <Card />
         </div>
       </div>
+     
       );
 };
 
